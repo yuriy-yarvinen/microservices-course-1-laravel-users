c3d7401b9b051ba316dff783fe3586c03ce76f2e
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2022-01-08 22:22:50 +0300

    finish user microservice

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\PaginatedResource;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -16,7 +18,27 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function customUsers(Request $request)
+    {
+        $data = $request->validate([
+            'is_influencer' => 'required',
+        ]);
+        
+        return User::where($data)->get();
     }
 
 
@@ -28,11 +50,18 @@ class UserController extends Controller
 
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+
+        $user->update($request->only(['first_name', 'last_name', 'email']));
+
+        return response($user, Response::HTTP_ACCEPTED);
     }
 
     public function destroy($id)
     {
-        //
+        User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +25,8 @@ Route::group([
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::apiResource('users', UserController::class);
+    
+    Route::post('customUsers', [UserController::class, 'customUsers']);
     
     Route::put('users/info', [AuthController::class, 'updateInfo']);
     Route::put('users/password', [AuthController::class, 'updatePassword']);
