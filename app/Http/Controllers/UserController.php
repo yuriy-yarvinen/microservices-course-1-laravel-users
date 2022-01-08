c9fe08e4f8c67c68bbb9ff9b6c25ab34481207f6
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2022-01-08 20:35:04 +0300

    get user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,46 +14,23 @@ class UserController extends Controller
         return PaginatedResource::collection(User::paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
-        //
+        return User::find($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
